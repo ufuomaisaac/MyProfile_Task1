064fa4c1d3cfe55e15385a73ad96e693f4cb870c
ufuomaisaac <ejemudaroufuoma@gmail.com>
2023-09-07 23:10:16 +0100

    webview feature now working

--- app/src/main/java/com/example/myprofile_task1/WebViewActivity.kt
+++ app/src/main/java/com/example/myprofile_task1/WebViewActivity.kt
@@ -2,10 +2,21 @@ package com.example.myprofile_task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import com.example.myprofile_task1.databinding.ActivityWebViewBinding
 
 class WebViewActivity : AppCompatActivity() {
+    lateinit var binding : ActivityWebViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_web_view)
+        binding = ActivityWebViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.loadUrl("https://github.com/ufuomaisaac")
+        binding.webView.settings.also {
+            it.javaScriptEnabled = true
+            it.setSupportZoom(true)
+        }
     }
 }

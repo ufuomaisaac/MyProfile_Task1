cac48fe74bc129a412dd79bb7f68420ee3911804
ufuomaisaac <ejemudaroufuoma@gmail.com>
2023-09-07 18:31:25 +0100

    webview WIP

--- app/src/main/java/com/example/myprofile_task1/MainActivity.kt
+++ app/src/main/java/com/example/myprofile_task1/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.myprofile_task1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myprofile_task1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.profileButton.setOnClickListener {
+            Intent(this, WebViewActivity::class.java).also {
+                startActivity(it)
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/myprofile_task1/WebViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myprofile_task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+    }
+}
